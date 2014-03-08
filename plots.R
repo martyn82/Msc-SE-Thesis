b5c74b69e74d8b00cc8c4b80ac3f302d482f8bf6
Martijn Endenburg <martijn.endenburg@gmail.com>
2014-03-08 09:36:51 +0100

    Plot interesting variables for same project

--- plots.R
+++ plots.R
@@ -1,48 +1,41 @@
 
 data.frame <- read.csv2("tools/csv/converted.csv")
 timecol <- "Date"
-varcol <- "LOC"
 idcol <- "ProjectId"
 namecol <- "ProjectName"
+varcols <- c("LOC", "Commit.LOC.Churn", "Active.Developers")
 projects <- unique(data.frame[[idcol]])
-ordertimefn <- as.numeric
-
-cm <- 0
 
 for(pid in projects) {
-  project <- subset(data.frame, data.frame$ProjectId==pid, select=c(namecol, timecol, varcol))
-  project.times <- as.Date(project[[timecol]])
-  project.vars <- as.numeric(project[[varcol]])
-  project.name <- unique(project[[namecol]])
+  for(varcol in varcols){
+    project <- subset(data.frame, data.frame$ProjectId==pid, select=c(namecol, timecol, varcol))
+    project.times <- as.Date(project[[timecol]])
+    project.name <- unique(project[[namecol]])
+    project.vars <- as.numeric(project[[varcol]])
   
-  value.last <- project.vars[end(project.vars)][1]
-  value.max <- max(project.vars)
-  value.mean <- mean(project.vars)
+    value.last <- project.vars[end(project.vars)][1]
+    value.max <- max(project.vars)
+    value.mean <- mean(project.vars)
   
-  if(value.mean > value.last){
-    cm <- cm + 1
-    print(project.name)
+    mark <- "O"
+  
+    if(value.last == value.max){
+      mark <- "N"
+    }
+  
+    jpeg(paste("plots/", timecol, ".", varcol, ".", project.name, ".", mark, ".jpg", sep=""))
+  
+    plot(
+      project.times,
+      project.vars,
+      type="l",
+      main=project.name,
+      xlab=timecol,
+      ylab=varcol
+    )
+    
+    lines(project.times, rep(value.mean, times=length(project.times)))
+  
+    dev.off()
   }
-  
-  mark <- "O"
-  
-  if(value.last == value.max){
-    mark <- "N"
-  }
-  
-  jpeg(paste("plots/", timecol, ".", varcol, ".", project.name, ".", mark, ".jpg", sep=""))
-  
-  plot(
-    project.times,
-    project.vars,
-    type="l",
-    main=project.name,
-    xlab=timecol,
-    ylab=varcol
-  )
-  lines(project.times, rep(value.mean, times=length(project.times)))
-  
-  dev.off()
 }
-
-print(cm)
