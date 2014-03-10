645dab501349017ef00ff0d41c80a5cdccad7ab2
Martijn Endenburg <martijn.endenburg@gmail.com>
2014-03-10 19:25:04 +0100

    Plots several metrics in 2 dimensions

--- scripts/plots.R
+++ scripts/plots.R
@@ -1,41 +1,51 @@
+library("hash")
 
 data.frame <- read.csv2("tools/csv/converted.csv")
-timecol <- "Date"
+
 idcol <- "ProjectId"
 namecol <- "ProjectName"
-varcols <- c("LOC", "Commit.LOC.Churn", "Active.Developers")
+timecols <- c("Date", "Active.Developers")
+
+varcols <- hash(keys=timecols)
+varcols[["Date"]] <- c("LOC", "Commit.LOC.Churn", "Active.Developers")
+varcols[["Active.Developers"]] <- c("Commit.LOC.Churn")
+
 projects <- unique(data.frame[[idcol]])
 
 for(pid in projects) {
-  for(varcol in varcols){
-    project <- subset(data.frame, data.frame$ProjectId==pid, select=c(namecol, timecol, varcol))
-    project.times <- as.Date(project[[timecol]])
-    project.name <- unique(project[[namecol]])
-    project.vars <- as.numeric(project[[varcol]])
-  
-    value.last <- project.vars[end(project.vars)][1]
-    value.max <- max(project.vars)
-    value.mean <- mean(project.vars)
-  
-    mark <- "O"
-  
-    if(value.last == value.max){
-      mark <- "N"
+  for (timecol in timecols){
+    for(varcol in varcols[[timecol]]){
+      project <- subset(data.frame, data.frame[[idcol]]==pid, select=c(namecol, timecol, varcol))
+      project.sorted <- project[order(project[[timecol]], project[[varcol]]),]
+
+      if(timecol == "Date"){
+        project.times <- as.Date(project.sorted[[timecol]])
+      }
+      else {
+        project.times <- as.numeric(project.sorted[[timecol]])
+      }
+
+      project.name <- unique(project.sorted[[namecol]])
+      project.vars <- as.numeric(project.sorted[[varcol]])
+
+      value.last <- project.vars[end(project.vars)][1]
+      value.max <- max(project.vars)
+      value.mean <- mean(project.vars)
+
+      jpeg(paste("plots/", timecol, ".", varcol, ".", project.name, ".", "jpg", sep=""))
+
+      plot(
+        project.times,
+        project.vars,
+        type="l",
+        main=project.name,
+        xlab=timecol,
+        ylab=varcol
+      )
+
+      lines(project.times, rep(value.mean, times=length(project.times)))
+
+      dev.off()
     }
-  
-    jpeg(paste("plots/", timecol, ".", varcol, ".", project.name, ".", mark, ".jpg", sep=""))
-  
-    plot(
-      project.times,
-      project.vars,
-      type="l",
-      main=project.name,
-      xlab=timecol,
-      ylab=varcol
-    )
-    
-    lines(project.times, rep(value.mean, times=length(project.times)))
-  
-    dev.off()
   }
 }
