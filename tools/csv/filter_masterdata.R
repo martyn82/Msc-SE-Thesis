6dc6781a45c83f1538164777189d8e9daccd1866
Martijn Endenburg <martijn.endenburg@gmail.com>
2014-03-31 16:15:28 +0200

    Added masterdata filter script

---
+++ tools/csv/filter_masterdata.R
@@ -0,0 +1,13 @@
+# This script filters the masterdata.txt for the sample selection based on projects existing in data/projectsWithCoverageForSample.csv
+
+input.file <- "tools/csv/masterdata.txt"
+source.file <- "data/projectsWithCoverageForSample.csv"
+
+output.file <- "tools/csv/masterdata_filtered.csv"
+
+input.df <- read.csv2(input.file, sep="\t")
+source.df <- read.csv2(source.file)
+
+output.df <- input.df[!input.df$id %in% source.df$project_id, ]
+
+write.csv2(output.df, file=output.file)
