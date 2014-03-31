efd9a1fabfb03cf9883ef23ef8694e1dc0145ac1
Martijn Endenburg <martijn.endenburg@gmail.com>
2014-03-31 22:11:36 +0200

    Selection of projects for sample by coverage and number of data points

--- tools/csv/generate_list_for_sample.R
+++ tools/csv/generate_list_for_sample.R
@@ -2,11 +2,12 @@
 # have a coverage from a given minimum up to 100%.
 
 coverage.min <- 1 # percentage divided by 100
+points.min <- 12  # minimum amount of data points
 
 input.file <- "data/projectsWithCoverage.csv"
 output.file <- "data/projectsWithCoverageForSample.csv"
 
-output.cols <- c("project_id", "project_name", "coverage")
+output.cols <- c("project_id", "project_name", "coverage", "points")
 output.df <- as.data.frame(
   matrix(ncol=length(output.cols), nrow=0)
 )
@@ -16,33 +17,48 @@ print("Reading input file...")
 input.df <- read.csv2(input.file)
 
 pids <- unique(input.df[["project_id"]])
-c <- 1
 total <- length(pids)
+stats.selected <- 0
+stats.skipped.coverage <- 0
+stats.skipped.size <- 0
+c <- 0
 
 for(pid in pids){
-  print(
-    paste("Processing project ID", pid, "(", c, "/", total, ")...")
-  )
+  c <- c + 1
 
+  print(
+    paste("Processing project ID", pid, "(", c, "/", total, ")")
+  )
+  
   project.data <- subset(input.df, input.df[["project_id"]]==pid)
   project.data.coverage <- project.data[["coverage"]]
+  project.data.size <- project.data[["sample_size"]]
 
   if(project.data.coverage < coverage.min){
+    stats.skipped.coverage <- stats.skipped.coverage + 1
     next
   }
   
+  if(project.data.size < points.min){
+    stats.skipped.size <- stats.skipped.size + 1
+    next
+  }
+
   row <- data.frame(
     project_id=pid,
     project_name=project.data[["project_name"]],
-    coverage=project.data.coverage
+    coverage=project.data.coverage,
+    points=project.data.size
   )
   
   output.df <- rbind(output.df, row)
-  
-  c <- c + 1
+  stats.selected <- stats.selected + 1
 }
 
 print("Writing to file...")
 write.csv2(output.df, file=output.file)
 
+print(paste("Projects selected:", stats.selected))
+print(paste("Projects skipped:", stats.skipped.coverage, "(low coverage)", stats.skipped.size, "(small)"))
+print(paste("Total projects:", total))
 print("Done.")
