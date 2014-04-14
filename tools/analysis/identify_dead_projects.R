43a54b2bed2213e14abd4b8a830452d14d810074
Martijn Endenburg <martijn.endenburg@gmail.com>
2014-04-14 17:33:21 +0200

    Improved LOC count of dead projects, added end.date of data

--- tools/analysis/identify_dead_projects.R
+++ tools/analysis/identify_dead_projects.R
@@ -9,7 +9,7 @@ input.file <- paste("data", "factsForAnalysis.csv", sep="/")
 input.data <- read.csv2(input.file)
 
 output.file <- paste("output", "deadProjects.csv", sep="/")
-output.cols <- c("pid", "by.loc", "by.churn", "by.devs")
+output.cols <- c("pid", "end.date", "by.loc", "by.churn", "by.devs")
 
 pids <- unique(input.data$Project.Id)
 dead.pids <- as.data.frame(
@@ -28,9 +28,9 @@ for(pid in pids){
 
   project.data.LOC.Churn <- sum(project.data.last_months$LOC.Churn) # If dead, the sum of churn will be low
   project.data.Active.Developers <- max(project.data.last_months$Active.Developers) # If dead, the max of active developers will be low
-  project.data.LOC <- max(project.data.last_months$LOC) - min(project.data.last_months$LOC) # If dead, the LOC diff will be low
+  project.data.LOC <- project.data.last_months[1, "LOC"] # If dead, the LOC diff will be low
 
-  project.is_dead.by.LOC <- (project.data.LOC <= dead.threshold.LOC)
+  project.is_dead.by.LOC <- all(project.data.last_months$LOC == project.data.LOC) #(project.data.LOC <= dead.threshold.LOC)
   project.is_dead.by.LOC.Churn <- (project.data.LOC.Churn <= dead.threshold.LOC.Churn)
   project.is_dead.by.Active.Developers <- (project.data.Active.Developers <= dead.threshold.Active.Developers)
   
@@ -38,6 +38,7 @@ for(pid in pids){
     dead.pids.index <- dead.pids.index + 1
     dead.pids[dead.pids.index, ] <- c(
       pid,
+      as.character(max(as.Date(project.data.last_months$Date))),
       format(project.is_dead.by.LOC),
       format(project.is_dead.by.LOC.Churn),
       format(project.is_dead.by.Active.Developers)
