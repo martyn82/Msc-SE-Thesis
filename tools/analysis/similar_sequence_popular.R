78f8ec11ed7a0b1c14d1431c2ec6b50b8be0717e
Martijn Endenburg <martijn.endenburg@gmail.com>
2014-04-28 15:47:55 +0200

    Use Active.Developers

--- tools/analysis/similar_sequence_popular.R
+++ tools/analysis/similar_sequence_popular.R
@@ -25,7 +25,7 @@ alivepids <- allpids[!(allpids %in% deadpids)]
 minocc <- 3
 filter_name <- "haar"
 #interesting_colnames <- c("Active.Developers", "Commit.LOC.Added", "Commit.LOC.Churn", "Commit.LOC.Modified", "Commit.LOC.Removed", "Cumulative.Developers", "Cumulative.LOC.Added", "Cumulative.LOC.Churn", "Cumulative.LOC.Modified", "Cumulative.LOC.Removed", "LOC", "Relative.Date.Progress", "Relative.LOC.Churn.Progress", "Relative.Team.Size", "Files", "Commits")
-interesting_colnames <- c("LOC.Churn")
+interesting_colnames <- c("Active.Developers")
 
 simfile <- file(paste(folder.root, paste("haar", "similar", "Age.Months", "LOC.Churn.csv", sep="_"), sep="/"), open="w")
 open(simfile)
