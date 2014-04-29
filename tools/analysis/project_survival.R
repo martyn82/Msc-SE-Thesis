59b12579e2462b3cfd0452c45163f7d7a339e602
Martijn Endenburg <martijn.endenburg@gmail.com>
2014-04-29 11:48:55 +0200

    Added survival scripts

---
+++ tools/analysis/project_survival.R
@@ -0,0 +1,52 @@
+library(plyr)
+library(splines)
+library(survival)
+
+source("tools/analysis/ggkm.R")
+
+metric <- "Active.Developers"
+
+facts.data <- read.csv2("data/factsForAnalysis.csv")
+dead.data <- read.csv2("output/deadProjectsValidated.csv")
+dying.data <- read.csv2(paste("output", paste(paste("dyingProjectsValidated", metric, sep="_"), "csv", sep="."), sep="/"))
+
+max.deadcount <- max(dying.data$dead.count)
+
+dead.data <- subset(dead.data, dead.data$confirmed.dead == "TRUE")
+dying.data <- subset(dying.data, dying.data$dead.count == max.deadcount)
+control.data <- subset(facts.data, !(facts.data$Project.Id %in% dying.data$pid))
+
+cols <- c("pid", "time", "group", "status")
+projects <- as.data.frame(
+  matrix(
+    ncol=length(cols),
+    nrow=0
+  )
+)
+
+dead.pids <- unique(dead.data$pid)
+dying.pids <- unique(dying.data$pid)
+control.pids <- unique(control.data$Project.Id)[1:length(dying.pids)]
+
+control.data <- subset(control.data, control.data$Project.Id %in% control.pids)
+all.pids <- append(dying.pids, control.pids)
+
+for(pid in all.pids){
+  project <- subset(facts.data, facts.data$Project.Id == pid)
+  is.dead <- (pid %in% dead.pids)
+  has.sequence <- (pid %in% dying.pids)
+
+  row <- c(
+    "pid"=pid,
+    "time"=max(project$Age.Months),
+    "group"=as.numeric(has.sequence),
+    "status"=as.numeric(!is.dead)
+  )
+
+  projects <- rbind(projects, row)
+}
+
+colnames(projects) <- cols
+
+surv.fit <- survfit(Surv(time, status) ~ group, data=projects)
+ggkm(surv.fit, timeby=12)
