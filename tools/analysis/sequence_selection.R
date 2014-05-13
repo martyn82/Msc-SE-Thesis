da123a6a94805cc397a04353b703c70e59fa7291
Martijn Endenburg <martijn.endenburg@gmail.com>
2014-05-13 12:02:40 +0200

    Added sequences of the metrics to sequence metadata

--- tools/analysis/sequence_selection.R
+++ tools/analysis/sequence_selection.R
@@ -18,7 +18,11 @@ stats <- data.frame(
   "seq.id"=numeric(0),
   "max.loc.diff"=numeric(0),
   "max.ad.diff"=numeric(0),
-  "max.lc.diff"=numeric(0)
+  "max.lc.diff"=numeric(0),
+  "loc.seq"=character(0),
+  "ad.seq"=character(0),
+  "lc.seq"=character(0),
+  stringsAsFactors=FALSE
 )
 i <- 1
 
@@ -30,23 +34,29 @@ for(s in 1:nrow(locs)){
 
   diff.loc <- max(abs(diff(project.seq$LOC)))
   has.diff.loc <- (max(diff.loc) != 0)
+  loc.seq <- paste(project.seq$LOC, sep="|", collapse="|")
 
   diff.ad <- max(abs(diff(project.seq$Active.Developers)))
   has.diff.ad <- (max(diff.ad) != 0)
+  ad.seq <- paste(project.seq$Active.Developers, sep="|", collapse="|")
 
   diff.lc <- max(abs(diff(project.seq$LOC.Churn)))
   has.diff.lc <- (max(diff.lc) != 0)
+  lc.seq <- paste(project.seq$LOC.Churn, sep="|", collapse="|")
 
   stats[i, ] <- c(
     "pid"=pid,
     "seq.id"=seq$seq.id,
     "max.loc.diff"=diff.loc,
     "max.ad.diff"=diff.ad,
-    "max.lc.diff"=diff.lc
+    "max.lc.diff"=diff.lc,
+    "loc.seq"=loc.seq,
+    "ad.seq"=ad.seq,
+    "lc.seq"=lc.seq
   )
   i <- i + 1
 }
-rm(diff.ad, diff.lc, diff.loc, has.diff.ad, has.diff.lc, has.diff.loc)
+rm(diff.ad, diff.lc, diff.loc, has.diff.ad, has.diff.lc, has.diff.loc, loc.seq, ad.seq, lc.seq)
 rm(pids, i, s, seq, pid, facts, locs, project, project.seq)
 
 output.file <- paste("output", paste(paste("sequenceMetaData", metric, sep="_"), "csv", sep="."), sep="/")
